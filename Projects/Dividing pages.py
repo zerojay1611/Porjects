1ba292e6febffac9b3e1726fa7e0011e04aa9f15
zerojay1611 <65505072+zerojay1611@users.noreply.github.com>
2020-06-22 02:54:27 +0900

    simulation version

--- Projects/Dividing pages.py
+++ Projects/Dividing pages.py
@@ -1,22 +1,47 @@
-def loss(day, p):
+def loss(p):
     s_ = 0
 
     for i in range(len(p)):     #sigma(avg - p)**2
         s_ += (avg - p[i])**2
     
     return s_
-'''
-def simulation(page, i, j, go):
-    tmp_i = i
-    tmp_j = j
-    while i < len(result):
-        while j < len(result[i]):
-            if result[i][j] == -1:
-                break
 
-            if tmp_i == i and tmp_j == j and go:
-                page[d] += result[i][j]
-    '''        
+def simulation(d_, i_, j_, go):
+    page = pages[:]
+
+    if go:
+        page[d_] += result[i_][j_]
+    else:
+        page.append(0)
+        d_ += 1
+
+        page[d_] += result[i_][j_]
+        j_ += 1
+
+    while i_ < len(result):
+
+        if i_ != 0:
+            j_ = 0
+        while j_ < len(result[i_]):
+            if result[i_][j_] == -1:
+                break
+            
+            p_ = page[:]
+            p_[d_] += result[i_][j_]
+
+            if loss(p_) < loss(page):
+                page[d_] += result[i_][j_]
+            elif d_ < (days - 1):
+                page.append(0)
+                d_ += 1
+
+                page[d_] += result[i_][j_]
+            
+            j_ += 1
+        i_ += 1
+    
+    return page
+
 
 
 data = [[11, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1], 
@@ -70,10 +95,13 @@ for i in range(len(data)):
         pages_ = pages[:]       #pages_ = pages 이렇게 하면 메모리 포인터 자체가 복사됨
         pages_[d] += result[i][j]
 
-        if loss(d+1, pages_) < loss(d+1, pages):
+        if pages[d] != 0 and loss(simulation(d, i, j, True)) < loss(simulation(d, i, j, False)):
             pages[d] += result[i][j]
             strs[d] += str(i+1) + '-' + str(j+1) +  ' '
-        elif d <= days:
+        elif pages[d] == 0:
+            pages[d] += result[i][j]
+            strs[d] += str(i+1) + '-' + str(j+1) +  ' '
+        elif d < (days - 1):
             pages.append(0)
             strs.append('')
             d += 1
