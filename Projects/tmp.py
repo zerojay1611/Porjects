47ba13b4c49558d709dbf9626f2e89528e5760c5
zerojay1611 <65505072+zerojay1611@users.noreply.github.com>
2020-06-22 03:13:37 +0900

    Add files via upload

--- Projects/tmp.py
+++ Projects/tmp.py
@@ -6,44 +6,6 @@ def loss(p):
     
     return s_
 
-def simulation(d_, i_, j_, go):
-    page = pages[:]
-
-    if go:
-        page[d_] += result[i_][j_]
-    else:
-        page.append(0)
-        d_ += 1
-
-        page[d_] += result[i_][j_]
-        j_ += 1
-
-    while i_ < len(result):
-
-        if i_ != 0:
-            j_ = 0
-        while j_ < len(result[i_]):
-            if result[i_][j_] == -1:
-                break
-            
-            p_ = page[:]
-            p_[d_] += result[i_][j_]
-
-            if loss(p_) < loss(page):
-                page[d_] += result[i_][j_]
-            elif d_ < (days - 1):
-                page.append(0)
-                d_ += 1
-
-                page[d_] += result[i_][j_]
-            
-            j_ += 1
-        i_ += 1
-    
-    return page
-
-
-#입력데이터
 data = [[11, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1], 
 [15, 18, 24, 26, 37, 44, 64, 65, 68, 71, 76], 
 [79, 86, 94, -1, -1, -1, -1, -1, -1, -1, -1], 
@@ -87,7 +49,6 @@ pages = [0]
 strs = ['']
 d = 0
 
-#시뮬레이션 방법
 for i in range(len(data)):
     for j in range(len(data[0])):   #지금과 늘리기 후를 (평균*지금날짜)를 기준으로 비교하고 늘리거나 통과
         if result[i][j] == -1:
@@ -96,13 +57,10 @@ for i in range(len(data)):
         pages_ = pages[:]       #pages_ = pages 이렇게 하면 메모리 포인터 자체가 복사됨
         pages_[d] += result[i][j]
 
-        if pages[d] != 0 and loss(simulation(d, i, j, True)) < loss(simulation(d, i, j, False)):
+        if loss(pages_) < loss(pages):
             pages[d] += result[i][j]
             strs[d] += str(i+1) + '-' + str(j+1) +  ' '
-        elif pages[d] == 0:
-            pages[d] += result[i][j]
-            strs[d] += str(i+1) + '-' + str(j+1) +  ' '
-        elif d < (days - 1):
+        elif d < (days-1):
             pages.append(0)
             strs.append('')
             d += 1
@@ -110,38 +68,15 @@ for i in range(len(data)):
             pages[d] += result[i][j]
             strs[d] += str(i+1) + '-' + str(j+1) +  ' '
 
-#기본 방법
+print(pages)
+print(strs)
 
-pages_b = [0]
-strs_b = ['']
-d_b = 0
-
-for i in range(len(data)):
-    for j in range(len(data[0])):   #지금과 늘리기 후를 (평균*지금날짜)를 기준으로 비교하고 늘리거나 통과
-        if result[i][j] == -1:
-            break
-
-        pages_ = pages_b[:]       #pages_ = pages 이렇게 하면 메모리 포인터 자체가 복사됨
-        pages_[d_b] += result[i][j]
-
-        if pages_b[d_b] != 0 and loss(pages_) < loss(pages_b):
-            pages_b[d_b] += result[i][j]
-            strs_b[d_b] += str(i+1) + '-' + str(j+1) +  ' '
-        elif pages_b[d_b] == 0:
-            pages_b[d_b] += result[i][j]
-            strs_b[d_b] += str(i+1) + '-' + str(j+1) +  ' '
-        elif d_b < (days - 1):
-            pages_b.append(0)
-            strs_b.append('')
-            d_b += 1
-
-            pages_b[d_b] += result[i][j]
-            strs_b[d_b] += str(i+1) + '-' + str(j+1) +  ' '
+#tmp
+basic7 = [61, 51, 49, 53, 50, 53, 41]
+sim7 = [61, 45, 51, 57, 50, 53, 41]
+sim8 = [40, 41, 42, 44, 47, 50, 53, 41]
 
 
-if loss(pages_b) < loss(pages):
-    print(pages_b)
-    print(strs_b)
-else:
-    print(pages)
-    print(strs)
+print(loss(1, sim7))
+print(loss(0, basic7))
+print(loss(0, sim8))
